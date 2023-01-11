d2ef6187b675aee708b588c79af7de3339f3325e
Owinnie <tillawinnie@gmail.com>
2023-01-11 18:20:34 +0300

    unordered lists

--- markdown2html.py
+++ markdown2html.py
@@ -14,10 +14,14 @@ if __name__ == "__main__":
         exit(1)
     else:
         with open(argv[1]) as md:
+            # ls = []
             for line in md:
-                if line.startswith("#"):
-                    # line.strip()
-                    with open(argv[2], "a") as htl:
+                ls = []
+                with open(argv[2], "a") as htl:
+
+                    # Headings
+
+                    if line.startswith("#"):
                         if line.count("#") <= 6:
                             (htl.write(line
                                        .replace("# ", "#")
@@ -32,4 +36,24 @@ if __name__ == "__main__":
                                                     .format(line
                                                             .count("#")
                                                             )))
+                    
+                    # unordered lists
+
+                    if line.startswith("-"):
+                        ls.append(line)
+
+                    for ln in ls:
+                        (htl.write(ln
+                                   .replace("- ", "-")
+                                   .strip()
+                                   .replace("-", "<li>") + "</li>\n"))
+        with open(argv[2], "r") as h:
+            cont = h.readlines()
+
+        cont.insert(1, "<ul>\n")
+        cont.append("</ul>\n")
+
+        with open(argv[2], "w") as h:
+            cont = "".join(cont)
+            h.write(cont)
         exit(0)
