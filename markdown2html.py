4cbc5f9b7b1ff1a010a0a99b2648cd2e9e0dc236
Owinnie <tillawinnie@gmail.com>
2023-01-11 14:37:36 +0300

    requirements achievement

--- markdown2html.py
+++ markdown2html.py
@@ -2,11 +2,11 @@
 """ Takes 2 string arguments """
 
 
-from sys import argv, stderr
+from sys import argv, exit, stderr
 import os
 
 if __name__ == "__main__":
-    if len(argv) < 2:
+    if len(argv[1:]) != 2:
         stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     elif os.path.exists(argv[1]) is False:
