7b4a57f3ca5c86e4f853afc5350e05cd5a79939c
Owinnie <tillawinnie@gmail.com>
2023-01-11 14:25:12 +0300

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" Takes 2 string arguments """
+
+
+from sys import argv, stderr
+import os
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit(1)
+    elif os.path.exists(argv[1]) is False:
+        stderr.write("Missing {}\n".format(argv[1]))
+        exit(1)
+    else:
+        # print()
+        exit(0)
